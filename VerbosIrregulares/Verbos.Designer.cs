53df29e776241c39d2e155b34987b83213464aa5
Daniel López <baphomet_dlh@me.com>
2024-04-02 13:38:09 +0200

    Agregar archivos de proyecto.

---
+++ VerbosIrregulares/Verbos.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace VerbosIrregulares
+{
+    partial class Verbos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblVerb = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(111, 252);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(127, 75);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "EMPEZAR";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(308, 252);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(119, 75);
+            this.btnFinish.TabIndex = 1;
+            this.btnFinish.Text = "TERMINAR";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click_1);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswer.Location = new System.Drawing.Point(111, 143);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(316, 38);
+            this.txtAnswer.TabIndex = 2;
+            this.txtAnswer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAnswer_KeyDown_1);
+            // 
+            // lblVerb
+            // 
+            this.lblVerb.AutoSize = true;
+            this.lblVerb.Font = new System.Drawing.Font("Courier New", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVerb.Location = new System.Drawing.Point(203, 45);
+            this.lblVerb.Name = "lblVerb";
+            this.lblVerb.Size = new System.Drawing.Size(150, 41);
+            this.lblVerb.TabIndex = 3;
+            this.lblVerb.Text = "label1";
+            this.lblVerb.Visible = false;
+            // 
+            // Verbos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 396);
+            this.Controls.Add(this.lblVerb);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Verbos";
+            this.Text = "Verbos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblVerb;
+    }
+}
+
---
+++ VerbosIrregulares/Verbos.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace VerbosIrregulares
+{
+    public partial class Verbos : System.Windows.Forms.Form
+    {
+        private List<Verb> verbs;
+        private Random random;
+        private int currentVerbIndex;
+        private int attempts;
+        private int correctAnswers;
+        private int incorrectAnswers;
+
+        public Verbos()
+        {
+            InitializeComponent();
+            InitializeExcel();
+            LoadVerbs();
+            random = new Random();
+        }
+
+        private void InitializeExcel()
+        {
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Abre el archivo Excel usando EPPlus
+            string excelPath = @"..\..\..\excel\verbosIrregulares.xlsx";
+            FileInfo excelFile = new FileInfo(excelPath);
+            using (ExcelPackage package = new ExcelPackage(excelFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["COMPLETO"];
+                int rowCount = worksheet.Dimension.Rows;
+                verbs = new List<Verb>();
+
+                for (int row = 2; row <= 46; row++)
+                {
+                    Verb verb = new Verb
+                    {
+                        Infinitive = worksheet.Cells[row, 1].Value.ToString(),
+                        SimplePast = worksheet.Cells[row, 2].Value.ToString(),
+                        PastParticiple = worksheet.Cells[row, 3].Value.ToString(),
+                        Meaning = worksheet.Cells[row, 4].Value.ToString()
+                    };
+                    verbs.Add(verb);
+                }
+            }
+        }
+
+        private void LoadVerbs()
+        {
+            // Carga los verbos desde el archivo Excel
+            // Implementación ya realizada en InitializeExcel()
+        }
+
+        private void ShowRandomVerb()
+        {
+            // Muestra un verbo aleatorio en la etiqueta lblVerb
+            currentVerbIndex = random.Next(verbs.Count);
+            lblVerb.Text = verbs[currentVerbIndex].Infinitive;
+        }
+
+        private void CheckAnswer(string answer)
+        {
+            // Verifica si la respuesta del usuario es correcta
+            Verb currentVerb = verbs[currentVerbIndex];
+            if (answer.ToLower() == currentVerb.SimplePast.ToLower())
+            {
+                MessageBox.Show("¡Correcto!");
+                correctAnswers++;
+                attempts = 0;
+                ShowRandomVerb();
+                txtAnswer.Clear();
+            }
+            else
+            {
+                attempts++;
+                if (attempts == 3)
+                {
+                    MessageBox.Show($"Respuesta incorrecta. La respuesta correcta es: {currentVerb.SimplePast}");
+                    incorrectAnswers++;
+                    attempts = 0;
+                    ShowRandomVerb();
+                    txtAnswer.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("ERROR");
+                    txtAnswer.Clear();
+                }
+            }
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            // Comienza el cuestionario
+            ShowRandomVerb();
+            btnStart.Enabled = false;
+            txtAnswer.Enabled = true;
+            txtAnswer.Visible = true;
+            btnFinish.Enabled = true;
+            lblVerb.Visible = true;
+        }
+
+        private void btnFinish_Click_1(object sender, EventArgs e)
+        {
+            // Muestra el resumen y cierra la aplicación
+            MessageBox.Show($"Palabras intentadas: {correctAnswers + incorrectAnswers}\n" +
+                            $"Respuestas correctas: {correctAnswers}\n" +
+                            $"Respuestas incorrectas: {incorrectAnswers}");
+            this.Close();
+        }
+
+        private void txtAnswer_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            // Verifica la respuesta del usuario cuando presiona Enter
+            if (e.KeyCode == Keys.Enter)
+            {
+                CheckAnswer(txtAnswer.Text);
+            }
+        }
+    }
+
+    public class Verb
+    {
+        // Modelo de datos para los verbos
+        public string Infinitive { get; set; }
+        public string SimplePast { get; set; }
+        public string PastParticiple { get; set; }
+        public string Meaning { get; set; }
+    }
+}
